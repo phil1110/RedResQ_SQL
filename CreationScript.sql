164e2299b332db267a97cc288a44c1e7045ebe1a
Philipp Recknagel <philipp.recknagel@edu.szu.at>
2023-10-05 13:30:55 +0200

    Added IDENTITY attribute to primary keys and fixed errors

--- CreationScript.sql
+++ CreationScript.sql
@@ -6,61 +6,61 @@ GO
 
 CREATE TABLE Location
 (
-    [ID] INT NOT NULL PRIMARY KEY,
+    [ID] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
     [country] VARCHAR(255) NOT NULL,
     [region] VARCHAR(255) NOT NULL,
     [place] VARCHAR(255) NOT NULL,
-    [postalcode] VARCHAR(255) NOT NULL
+    [postalCode] VARCHAR(255) NOT NULL
 );
 GO
 
 CREATE TABLE Language
 (
-    [ID] INT NOT NULL PRIMARY KEY,
+    [ID] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
     [name] VARCHAR(255) NOT NULL
 );
 GO
 
 CREATE TABLE Image
 (
-    [ID] INT NOT NULL PRIMARY KEY,
+    [ID] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
     [source] VARCHAR(255) NOT NULL
 );
 GO
 
 CREATE TABLE Quiz
 (
-    [ID] INT NOT NULL PRIMARY KEY,
+    [ID] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
     [name] VARCHAR(10) NOT NULL,
-    [maxscore] int NOT NULL
+    [maxScore] int NOT NULL
 );
 GO
 
 CREATE TABLE Question
 (
-    [ID] INT NOT NULL PRIMARY KEY,
+    [ID] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
     [text] VARCHAR(20) NOT NULL,
-    [QuizID] INT FOREIGN KEY REFERENCES Quiz(ID) NOT NULL
+    [QuizID] INT NOT NULL FOREIGN KEY REFERENCES Quiz(ID)
 );
 GO
 
 CREATE TABLE Answer
 (
-    [ID] INT NOT NULL PRIMARY KEY,
+    [ID] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
     [text] VARCHAR(10) NOT NULL,
-    [istrue] bit NOT NULL,
-    [QuestionID] INT FOREIGN KEY REFERENCES Question(ID) NOT NULL
+    [isT rue] bit NOT NULL,
+    [QuestionID] INT NOT NULL FOREIGN KEY REFERENCES Question(ID)
 );
 GO
 
 CREATE TABLE Article
 (
-    [ID] int not null primary key,
+    [ID] int not null primary key IDENTITY(1,1),
     [title] VARCHAR(255) not null,
     [content] VARCHAR(255) not null,
     [author] varchar(255) not null,
     [date] date not null,
-    [LanguageID] int FOREIGN key REFERENCES [Language](ID) not null,
+    [LanguageID] int not null FOREIGN key REFERENCES [Language](ID),
     [ImageID] int foreign key references [Image](ID),
     [LocationID] int FOREIGN KEY REFERENCES [Location](ID)
 );
@@ -68,7 +68,7 @@ GO
 
 create table Account
 (
-    [ID] int not null PRIMARY key,
+    [ID] int not null PRIMARY key IDENTITY(1,1),
     [username] VARCHAR(20) not null,
     [firstname] VARCHAR(20),
     [lastname] VARCHAR(20),
@@ -76,7 +76,7 @@ create table Account
     [birthdate] date not null,
     [password] varchar(30) not null,
     [sex] varchar(10) not null,
-    [LanguageID] int FOREIGN key REFERENCES [Language](ID) not null,
+    [LanguageID] int not null FOREIGN key REFERENCES [Language](ID),
     [LocationID] int FOREIGN KEY REFERENCES [Location](ID)
 );
 GO
