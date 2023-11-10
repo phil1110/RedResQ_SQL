3704e9137802db64477bf544834e3bd70a24de98
phil1110 <philipp.recknagel@edu.szu.at>
2023-11-10 19:05:11 +0100

    Created new view for the Login

--- Views.sql
+++ Views.sql
@@ -1,2 +1,14 @@
 use RedResQ
 GO;
+
+create view V_Login
+as
+select *
+from Person p
+left join Language lg
+on p.LanguageID = lg.ID
+left join Location lo
+on p.LocationID = lo.ID
+left join Role r
+on p.RoleID = r.ID
+GO;
