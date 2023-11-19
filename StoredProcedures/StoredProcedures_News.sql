af240232b56289b5de4658649acae9f02cbe093a
phil1110 <philipp.recknagel@edu.szu.at>
2023-11-19 23:46:24 +0100

    New SPs for Getting articles and one for deleting them

--- StoredProcedures/StoredProcedures_News.sql
+++ StoredProcedures/StoredProcedures_News.sql
@@ -1,6 +1,3 @@
-use RedResQ
-go
-
 create procedure SP_Ne_LatestArticles_Global
     @articleId int = null
 as
@@ -21,14 +18,14 @@ go
 
 create procedure SP_Ne_LatestArticles_Country
     @country VARCHAR(500),
-    @articleId int
+    @articleId int = null
 as
 if (@articleId is null)
 begin
     select top 10 *
     from V_Article va
     where va.country = @country
-    order by va.ArticleID desc;
+    order by va.ArticleID desc
 end
 else
 begin
@@ -39,3 +36,69 @@ begin
     order by va.ArticleID desc;
 end
 go
+
+create procedure SP_Ne_LatestArticles_Language
+    @language VARCHAR(500),
+    @articleId int = null
+as
+if (@articleId is null)
+begin
+    select top 10 *
+    from V_Article va
+    where va.name = @language
+    order by va.ArticleID desc
+end
+else
+begin
+    select top 10 *
+    from V_Article va
+    where va.name = @language
+    and va.ArticleID < @articleId
+    order by va.ArticleID desc;
+end
+go
+
+create procedure SP_Ne_LatestArticles_CountryAndLanguage
+    @country VARCHAR(500),
+    @language VARCHAR(500),
+    @articleId int = null
+as
+if (@articleId is null)
+begin
+    select top 10 *
+    from V_Article va
+    where va.name = @language
+    and va.country = @country
+    order by va.ArticleID desc
+end
+else
+begin
+    select top 10 *
+    from V_Article va
+    where va.name = @language
+    and va.country = @country
+    and va.ArticleID < @articleId
+    order by va.ArticleID desc;
+end
+go
+
+create procedure SP_Ne_NewArticle
+    @title varchar(255),
+    @content varchar(max),
+    @author varchar(255),
+    @date date,
+    @languageId int,
+    @imageId int,
+    @locationId int
+as
+    insert into Article (title, content, author, date, LanguageID, ImageID, LocationID)
+    VALUES (@title, @content, @author, @date, @languageId, @imageId, @locationId)
+go;
+
+create procedure SP_Ne_DeleteArticle
+    @id int
+as
+delete
+from Article
+where ID = @id
+go;
