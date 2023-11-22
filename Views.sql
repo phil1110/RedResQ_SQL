4b64b8dfce042f4926645f1114f23add001e0c39
Philipp Recknagel <philipp.recknagel@edu.szu.at>
2023-11-22 13:45:08 +0100

    Removed the joins in the login view as they arent needed anymore

--- Views.sql
+++ Views.sql
@@ -1,16 +1,7 @@
 create view V_Login
 as
-select p.ID as 'PersonID', p.username, p.firstname, p.lastname, p.email, p.birthdate, p.hash, p.sex,
-       lg.ID as 'LanguageID', lg.name as 'LanguageName',
-       lo.ID as 'LocationID', lo.country, lo.city, lo.postalCode,
-       r.ID as 'RoleID', r.name as 'RoleName'
+select p.*
 from Person p
-left join Language lg
-on p.LanguageID = lg.ID
-left join Location lo
-on p.LocationID = lo.ID
-left join Role r
-on p.RoleID = r.ID
 GO;
 
 create view V_Article
