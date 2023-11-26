cbac19bcec249a932ae1cd474a6c15c083153259
phil1110 <philipp.recknagel@edu.szu.at>
2023-11-26 16:04:57 +0100

    added a join for country in the Article View

--- Views.sql
+++ Views.sql
@@ -7,11 +7,13 @@ GO;
 create view V_Article
 as
 select a.ID as [ArticleID], a.title, a.content, a.author, a.date, ln.ID as [LanguageID], ln.name, im.ID as [ImageID],
-       im.source, lo.ID as [LocationID], lo.country, lo.city, lo.postalCode
+       im.source, lo.ID as [LocationID], lo.city, lo.postalCode, co.ID as [CountryID], co.country
 from Article a
 left join Language ln
 on a.LanguageID = ln.ID
 left join Image im
 on a.ImageID = im.ID
 left join Location lo
-on a.LocationID = lo.ID;
+on a.LocationID = lo.ID
+left join Country co
+on lo.CountryID = co.ID;
