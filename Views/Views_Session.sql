5386c3a8a1aee56d71780f221412dd1cda6eb945
phil1110 <philipp.recknagel@edu.szu.at>
2023-11-28 14:24:06 +0100

    Moved the views the their own Scripts

--- Views/Views_Session.sql
+++ Views/Views_Session.sql
@@ -15,17 +15,3 @@ on lo.CountryID = co.ID
 left join Role r
 on p.RoleID = r.ID
 GO;
-
-create view V_Article
-as
-select a.ID as [ArticleID], a.title, a.content, a.author, a.date, ln.ID as [LanguageID], ln.name, im.ID as [ImageID],
-       im.source, lo.ID as [LocationID], lo.city, lo.postalCode, co.ID as [CountryID], co.country
-from Article a
-left join Language ln
-on a.LanguageID = ln.ID
-left join Image im
-on a.ImageID = im.ID
-left join Location lo
-on a.LocationID = lo.ID
-left join Country co
-on lo.CountryID = co.ID;
