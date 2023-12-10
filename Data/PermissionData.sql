fcad141ff5c9bfabf8e289a84d42f289bb86b199
phil1110 <philipp.recknagel@edu.szu.at>
2023-12-10 15:43:09 +0100

    Finished the SPs and permissions needed for the country API-calls

--- Data/PermissionData.sql
+++ Data/PermissionData.sql
@@ -3,4 +3,7 @@ VALUES
     ('publishArticle', 4),
     ('editArticle', 4),
     ('deleteArticle', 5),
-    ('updatePermission', 6);
+    ('updatePermission', 6),
+    ('addCountry', 6),
+    ('editCountry', 6),
+    ('deleteCountry', 6);
--- StoredProcedures/StoredProcedures_Country.sql
+++ StoredProcedures/StoredProcedures_Country.sql
@@ -5,9 +5,35 @@ as
     order by c.country asc
 go;
 
+create procedure SP_Co_GetCountry
+    @id bigint
+as
+    select *
+    from Country c
+    where c.ID = @id
+    order by c.country asc
+go;
+
+
 create procedure SP_Co_AddCountry
     @countryName varchar(500)
 as
     insert into Country(country)
-    values @countryName
+    values (@countryName)
+go;
+
+create procedure SP_Co_EditCountry
+    @id bigint,
+    @countryName varchar(500)
+as
+    update Country
+    set country = @countryName
+    where ID = @id
+go;
+
+create procedure SP_Co_DeleteCountry
+    @id bigint
+as
+    Delete from Country
+    where ID = @id
 go;
