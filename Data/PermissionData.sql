36d08d1c4bebd7cbcc1e1423b4357bd3987d9db7
phil1110 <philipp.recknagel@edu.szu.at>
2023-12-12 21:58:48 +0100

    Added all SPs needed for the location datatype

--- Data/PermissionData.sql
+++ Data/PermissionData.sql
@@ -7,9 +7,15 @@ VALUES
     ('addCountry', 6),
     ('editCountry', 6),
     ('deleteCountry', 6),
+    ('getGender', 1),
     ('addGender', 5),
     ('editGender', 5),
     ('deleteGender', 5),
     ('addLanguage', 5),
     ('editLanguage', 5),
-    ('deleteLanguage', 5);
+    ('deleteLanguage', 5),
+    ('getLocation', 1),
+    ('searchLocation', 1),
+    ('addLocation', 1),
+    ('editLocation', 5),
+    ('deleteLocation', 5);
--- StoredProcedures/StoredProcedures_Location.sql
+++ StoredProcedures/StoredProcedures_Location.sql
@@ -6,6 +6,18 @@ as
     where v.ID = @id
 go;
 
+create procedure SP_Lo_SearchForLocation
+    @city varchar(255),
+    @postalCode varchar(255),
+    @countryId bigint
+as
+    select lo.ID
+    from Location lo
+    where lo.city = @city
+    and lo.postalCode = @postalCode
+    and lo.CountryID = @countryId
+go;
+
 create procedure SP_Lo_GetLocations
     @id bigint = null
 as
@@ -22,4 +34,47 @@ as
         where v.ID > @id
         order by v.ID asc;
     end
+go;
+
+create procedure SP_Lo_AddLocation
+    @city varchar(255),
+    @postalCode varchar(255),
+    @countryId bigint
+as
+    insert into Location (city, postalCode, CountryID)
+    values (@city, @postalCode, @countryId)
+go;
+
+create procedure SP_Lo_EditLocation
+    @id bigint,
+    @city varchar(255) = null,
+    @postalCode varchar(255) = null,
+    @countryId bigint = null
+as
+    if (@city is not null)
+        begin
+            update Location
+            set city = @city
+            where ID = @id;
+        end
+    if (@postalCode is not null)
+        begin
+            update Location
+            set postalCode = @postalCode
+            where ID = @id
+        end
+    if (@countryId is not null)
+        begin
+            update Location
+            set CountryID = @countryId
+            where ID = @id
+        end
+go;
+
+create procedure SP_Lo_DeleteLocation
+    @id bigint
+as
+    delete
+    from Location
+    where ID = @id
 go;
