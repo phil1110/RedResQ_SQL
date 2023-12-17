a949279bd81ff7dbf38f1ce14b7a8450e36d5688
Philipp Recknagel <philipp.recknagel@edu.szu.at>
2023-12-17 14:44:49 +0100

    Added new perms

--- Data/PermissionData.sql
+++ Data/PermissionData.sql
@@ -1,16 +1,25 @@
 insert into Permission (PermissionName, RoleID)
 VALUES
+    -- News related Permissions
     ('publishArticle', 3),
     ('editArticle', 3),
     ('deleteArticle', 5),
+
+    -- Permission related Permissions
     ('updatePermission', 6),
+
+    -- Country related Permissions
     ('addCountry', 6),
     ('editCountry', 6),
     ('deleteCountry', 6),
+
+    -- Gender related Permissions
     ('getGender', 1),
     ('addGender', 5),
     ('editGender', 5),
     ('deleteGender', 5),
+
+    -- Language related Permissions
     ('addLanguage', 5),
     ('editLanguage', 5),
     ('deleteLanguage', 5),
@@ -19,6 +28,8 @@ VALUES
     ('addLocation', 1),
     ('editLocation', 5),
     ('deleteLocation', 5),
+
+    -- User related Permissions
     ('fetchUsers', 4),
     ('getAnyUser', 4),
     ('getPersonalUser', 2),
@@ -27,4 +38,10 @@ VALUES
     ('promoteToEditor', 5),
     ('promoteToModerator', 5),
     ('promoteToAdministrator', 6),
-    ('promoteToSuperAdministrator', 6);
+    ('promoteToSuperAdministrator', 6),
+    ('searchForUser', 4),
+    ('checkExistenceOfEmailOrUsername', 1),
+
+    -- Role related Permissions
+    ('getRole', 5),
+    ('fetchRoles', 5);
