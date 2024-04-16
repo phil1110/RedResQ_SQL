30100a8fedb63939093970d52e4f19eff96e8ebd
Philipp Recknagel <philipp.recknagel@edu.szu.at>
2024-04-16 15:34:01 +0200

    changed the string name for Sta

--- Data/StatTypes.sql
+++ Data/StatTypes.sql
@@ -1,8 +1,12 @@
 use RedResQ
 go
 
+-- 1 for Bar
+-- 2 for Pie
+-- 3 for Line
+
 INSERT INTO StatType ([name])
 VALUES
     -- Quiz StatTypes
-    ('Quiz_LeastAttempts'),
-    ('Quiz_MostAttempts');
+    ('Quiz_1_LeastAttempts'),
+    ('Quiz_1_MostAttempts');
